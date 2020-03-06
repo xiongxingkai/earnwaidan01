86972dbccfd004909ba9233df48551d64bc94878
xiongxingkai <865828782@qq.com>
2020-03-06 18:32:11 +0800

    js方法总结

--- LearnExperience/js功能方法总结.js
+++ LearnExperience/js功能方法总结.js
@@ -599,3 +599,7 @@ function FilterXss(content) {
 // 反序列化取，序列化存
 const love = JSON.parse(localStorage.getItem("love"));
 localStorage.setItem("love", JSON.stringify("I Love You"));
+
+// 66.输出一个键盘
+(_=>[..."`1234567890-=~~QWERTYUIOP[]\\~ASDFGHJKL;'~~ZXCVBNM,./~"].map(x=>(o+=`/${b='_'.repeat(w=x<y?2:' 667699'[x=["Bs","Tab","Caps","Enter"][p++]||'Shift',p])}\\|`,m+=y+(x+'    ').slice(0,w)+y+y,n+=y+b+y+y,l+=' __'+b)[73]&&(k.push(l,m,n,o),l='',m=n=o=y),m=n=o=y='|',p=l=k=[])&&k.join`
+`)()
